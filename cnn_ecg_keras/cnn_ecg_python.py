5588135091595d660c21cfe4e297e7c280ec2aac
Sanda <macbookpro@Sandas-MacBook-Pro.local>
2022-11-08 18:30:12 -0500

    new run file

---
+++ cnn_ecg_keras/cnn_ecg_python.py
@@ -0,0 +1,283 @@
+import numpy as np
+import pandas as pd
+import os
+import h5py
+import matplotlib
+from matplotlib import pyplot as plt
+%matplotlib inline
+matplotlib.style.use('ggplot')
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+# Keras
+import keras
+from keras.models import Sequential
+from keras import layers
+from keras import optimizers
+from keras import backend as K
+from keras import regularizers
+
+# Tensorflow
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
+# Custom imports
+from physionet_processing import (fetch_h5data, spectrogram, 
+                                  special_parameters, transformed_stats)
+
+from physionet_generator import DataGenerator
+
+print('Tensorflow version:', tf.__version__)
+print('Keras version:', keras.__version__)
+
+#Open hdf5 file, load the labels and define training/validation splits
+
+# Data folder and hdf5 dataset file
+data_root = os.path.normpath('.')
+#data_root = os.path.normpath('/media/sf_vbshare/physionet_data/')
+#data_root = os.path.normpath('/home/ubuntu/projects/csproject')
+# hd_file = os.path.join(data_root, 'physio.h5')
+hd_file = "/Users/macbookpro/Documents/physio.h5"
+label_file = os.path.join(data_root, 'REFERENCE-v3.csv')
+
+# Open hdf5 file
+h5file =  h5py.File(hd_file, 'r')
+
+# Get a list of dataset names 
+dataset_list = list(h5file.keys())
+
+# Load the labels
+label_df = pd.read_csv(label_file, header = None, names = ['name', 'label'])
+# Filter the labels that are in the small demo set
+label_df = label_df[label_df['name'].isin(dataset_list)]
+
+# Encode labels to integer numbers
+label_set = list(sorted(label_df.label.unique()))
+encoder = LabelEncoder().fit(label_set)
+label_set_codings = encoder.transform(label_set)
+label_df = label_df.assign(encoded = encoder.transform(label_df.label))
+
+print('Unique labels:', encoder.inverse_transform(label_set_codings))
+print('Unique codings:', label_set_codings)
+print('Dataset labels:\n', label_df.iloc[100:110,])
+
+# Split the IDs in training and validation set
+test_split = 0.33
+idx = np.arange(label_df.shape[0])
+id_train, id_val, _, _ = train_test_split(idx, idx, 
+                                         test_size = test_split,
+                                         shuffle = True,
+                                         random_state = 123)
+
+# Store the ids and labels in dictionaries
+partition = {'train': list(label_df.iloc[id_train,].name), 
+             'validation': list(label_df.iloc[id_val,].name)}
+
+labels = dict(zip(label_df.name, label_df.encoded))
+
+#set up batch generator
+# Parameters needed for the batch generator
+
+# Maximum sequence length
+max_length = 18286
+
+# Output dimensions
+sequence_length = max_length
+spectrogram_nperseg = 64 # Spectrogram window
+spectrogram_noverlap = 32 # Spectrogram overlap
+n_classes = len(label_df.label.unique())
+batch_size = 32
+
+# calculate image dimensions
+data = fetch_h5data(h5file, [0], sequence_length)
+_, _, Sxx = spectrogram(data, nperseg = spectrogram_nperseg, noverlap = spectrogram_noverlap)
+dim = Sxx[0].shape
+print(dim)
+print(Sxx.shape)
+# print('Maximum sequence length:', max_length)
+
+
+
+params = {'batch_size': batch_size,
+          'dim': dim,
+          'nperseg': spectrogram_nperseg,
+          'noverlap': spectrogram_noverlap,
+          'n_channels': 1,
+          'sequence_length': sequence_length,
+          'n_classes': n_classes,
+          'shuffle': True}
+
+train_generator = DataGenerator(h5file, partition['train'], labels, augment = True, **params)
+val_generator = DataGenerator(h5file, partition['validation'], labels, augment = False, **params)
+
+for i, batch in enumerate(train_generator):
+    if i == 1:
+        break
+
+X = batch[0]
+y = batch[1]
+
+print('X shape:', X.shape)
+print('y shape:', y.shape)
+print('X type:', np.dtype(X[0,0,0,0]))
+
+#batch generator
+
+def imshow_batch(X, y, batch_idx):
+    
+    batch_labels = ['Class label:' + str(np.argmax(y[idx,])) for idx in batch_idx]
+    #batch_labels = ['Class label:' + str(np.argmax(y[idx,])[0]) for idx in batch_idx]
+
+    fig, ax = plt.subplots(1, len(batch_idx), figsize = (15, 3))
+
+    for i, idx in enumerate(batch_idx):
+    
+        ax[i].imshow(X[idx, :, :, 0].transpose(), cmap = 'jet', aspect = 'auto')
+        ax[i].grid(False)
+        #ax[i].axis('off')
+        ax[i].invert_yaxis()
+        ax[i].set(title = batch_labels[i])
+    
+    plt.show()
+        
+    return fig
+
+batch_idx = [0, 1, 2, 3, 4]
+fig = imshow_batch(X, y, batch_idx)
+plt.show()
+
+
+#model define
+
+# Convolutional blocks
+def conv2d_block(model, depth, layer_filters, filters_growth, 
+                 strides_start, strides_end, input_shape, first_layer = False):
+    
+    ''' Convolutional block. 
+    depth: number of convolutional layers in the block (4)
+    filters: 2D kernel size (32)
+    filters_growth: kernel size increase at the end of block (32)
+    first_layer: provide input_shape for first layer'''
+    
+    # Fixed parameters for convolution
+    conv_parms = {'kernel_size': (3, 3),
+                  'padding': 'same',
+                  'dilation_rate': (1, 1),
+                  'activation': None,
+                  'data_format': 'channels_last',
+                  'kernel_initializer': 'glorot_normal'}
+
+    for l in range(depth):
+
+        if first_layer:
+            
+            # First layer needs an input_shape 
+            model.add(layers.Conv2D(filters = layer_filters,
+                                    strides = strides_start,
+                                    input_shape = input_shape, **conv_parms))
+            first_layer = False
+        
+        else:
+            # All other layers will not need an input_shape parameter
+            if l == depth - 1:
+                # Last layer in each block is different: adding filters and using stride 2
+                layer_filters += filters_growth
+                model.add(layers.Conv2D(filters = layer_filters,
+                                        strides = strides_end, **conv_parms))
+            else:
+                model.add(layers.Conv2D(filters = layer_filters,
+                                        strides = strides_start, **conv_parms))
+        
+        # Continue with batch normalization and activation for all layers in the block
+        model.add(layers.BatchNormalization(center = True, scale = True))
+        model.add(layers.Activation('relu'))
+    
+    return model
+
+def MeanOverTime():
+    lam_layer = layers.Lambda(lambda x: K.mean(x, axis=1), output_shape=lambda s: (1, s[2]))
+    return lam_layer
+
+
+# Define the model
+# Model parameters
+filters_start = 32 # Number of convolutional filters
+layer_filters = filters_start # Start with these filters
+filters_growth = 32 # Filter increase after each convBlock
+strides_start = (1, 1) # Strides at the beginning of each convBlock
+strides_end = (2, 2) # Strides at the end of each convBlock
+depth = 4 # Number of convolutional layers in each convBlock
+n_blocks = 6 # Number of ConBlocks
+n_channels = 1 # Number of color channgels
+input_shape = (*dim, n_channels) # input shape for first layer
+
+
+model = Sequential()
+
+for block in range(n_blocks):
+
+    # Provide input only for the first layer
+    if block == 0:
+        provide_input = True
+    else:
+        provide_input = False
+    
+    model = conv2d_block(model, depth,
+                         layer_filters,
+                         filters_growth,
+                         strides_start, strides_end,
+                         input_shape,
+                         first_layer = provide_input)
+    
+    # Increase the number of filters after each block
+    layer_filters += filters_growth
+
+
+
+# Remove the frequency dimension, so that the output can feed into LSTM
+# Reshape to (batch, time steps, filters)
+model.add(layers.Reshape((-1, 224)))
+model.add(layers.core.Masking(mask_value = 0.0))
+model.add(MeanOverTime())
+
+# Alternative: Replace averaging by LSTM
+
+# Insert masking layer to ignore zeros
+#model.add(layers.core.Masking(mask_value = 0.0))
+
+# Add LSTM layer with 3 neurons
+#model.add(layers.LSTM(200))
+#model.add(layers.Flatten())
+
+# And a fully connected layer for the output
+model.add(layers.Dense(4, activation='sigmoid', kernel_regularizer = regularizers.l2(0.1)))
+
+
+model.summary()
+
+
+import datetime 
+# Compile the model and run a batch through the network
+model.compile(loss='categorical_crossentropy',
+              optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+              metrics=['acc'])
+
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+
+h = model.fit_generator(generator = train_generator,
+                              steps_per_epoch = 50,
+                              epochs = 300,
+                              validation_data = val_generator,
+                              validation_steps = 50, callbacks=[tensorboard_callback])
+
+
+
+df = pd.DataFrame(h.history)
+df.head()
+df.to_csv('/scratch/thurasx/ecg_project_2/cnn_ecg_kerascnn_ecg_keras/history.csv')
+
+
