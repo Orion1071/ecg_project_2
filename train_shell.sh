c583b388262a9a2399ed084a473c2e2cb6b687d3
Sanda <macbookpro@Sandas-MBP.home>
2022-11-08 15:21:45 -0500

    train model again

--- train_shell.sh
+++ train_shell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# tsp -m python /scratch/thurasx/ecg_project_2/codes/train.py 
-python /scratch/thurasx/ecg_project_2/codes/train.py 
+tsp -m python /scratch/thurasx/ecg_project_2/codes/train.py 
+# python /scratch/thurasx/ecg_project_2/codes/train.py 
